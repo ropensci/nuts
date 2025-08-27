8637f59e6913ce9d6403a3f96e0c3fdd4b13c8f0
AAoritz <moritz.hennicke@posteo.de>
2025-08-27 22:38:05 +0200

    new transition matrices with version 2024 and UK fix

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+  version <- utils::packageVersion(pkgname)
+
+  packageStartupMessage(
+    "If you use this package in your work, please support the development of open science by citing:
+
+    • ", cli::col_blue("Hennicke M, Krause W (2024). nuts: Convert European Regional Data. doi:10.5281/zenodo.10573056, R package version 1.1.0, <https://docs.ropensci.org/nuts/>"
+    )
+  )
+}
---
+++ data-raw/contributors.R
@@ -0,0 +1,25 @@
+# Add contributors
+usethis::use_author(
+  given = "Filipe",
+  family = "Batista e Silva",
+  role = "ctb",
+  comment = c(ORCID = "0000-0002-8752-6464")
+)
+
+usethis::use_author(
+  given = "Carmelo",
+  family = "Attardo",
+  role = "ctb"
+)
+
+usethis::use_author(
+  given = "Cristian",
+  family = "Pigaiani",
+  role = "ctb"
+)
+
+usethis::use_author(
+  given = "Michele",
+  family = "Tucci",
+  role = "ctb"
+)
