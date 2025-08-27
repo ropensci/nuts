3c14d90bd99efee605dd58e7518859c93dc0dc83
AAoritz <moritz.hennicke@posteo.de>
2025-08-27 22:51:07 +0200

    no non ASCII chars in start up message

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   packageStartupMessage(
     "If you use this package in your work, please support the development of open science by citing:
 
-    • ", cli::col_blue("Hennicke M, Krause W (2024). nuts: Convert European Regional Data. doi:10.5281/zenodo.10573056, R package version 1.1.0, <https://docs.ropensci.org/nuts/>"
+    ", cli::col_blue("Hennicke M, Krause W (2024). nuts: Convert European Regional Data. doi:10.5281/zenodo.10573056, R package version 1.1.0, <https://docs.ropensci.org/nuts/>"
     )
   )
 }
